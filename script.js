f9a8c28262a160c4090f2d32797149af4b96f518
Elucode <125608503+Elulove1@users.noreply.github.com>
2024-06-18 23:01:26 -0400

    AdventurerFactory class
     An AdventurerFactory class that can be used to generate and manage adventurers of a specific role is added.

--- script.js
+++ script.js
@@ -212,3 +212,29 @@ class Adventurer extends Character {
     }
 }
 
+// Part 5: Gather your Party
+// An AdventurerFactory class that can be used to generate and manage adventurers of a specific role
+
+class AdventurerFactory {
+    constructor(role) {
+        this.role = role;
+        this.adventurers = [];
+    }
+
+    generate(name) {
+        const newAdventurer = new Adventurer(name, this.role);
+        this.adventurers.push(newAdventurer);
+        return newAdventurer;
+    }
+
+    findByIndex(index) {
+        return this.adventurers[index];
+    }
+
+    findByName(name) {
+        return this.adventurers.find((a) => a.name === name);
+    }
+}
+
+const healerFactory = new AdventurerFactory("Healer");
+const robin = healerFactory.generate("Robin");
